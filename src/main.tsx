8434e48ea814e7f58889354462735b17a630bb83
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-01-02 15:29:08 +0900

    Chore: Prepare Demo

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import { Provider } from "@/components/ui/provider.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { OnboardingPage } from "./components/pages/OnboardingPage.tsx";
 import { DiagnosisPage } from "./components/pages/DiagnosisPage.tsx";
-import ReportPage from "./components/pages/ReportPage.tsx";
+// import ReportPage from "./components/pages/ReportPage.tsx";
 import ReportPageV2 from "./components/pages/ReportPageV2.tsx";
 
 // strict mode for check error
-const isTEST = true;
+const isTEST = false;
 
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")!).render(
                     element={isTEST ? <ReportPageV2 /> : <OnboardingPage />}
                 />
                 <Route path="diagnosis" element={<DiagnosisPage />} />
-                <Route path="report" element={<ReportPage />} />
+                <Route path="report" element={<ReportPageV2 />} />
             </Routes>
         </Provider>
     </BrowserRouter>
