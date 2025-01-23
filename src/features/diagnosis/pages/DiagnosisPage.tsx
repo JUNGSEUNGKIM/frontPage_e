9308ef92871c6958a3aa00c0b9c3e07e42aef7c0
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-01-23 17:14:33 +0900

    fix : fix layout issue

--- src/features/diagnosis/pages/DiagnosisPage.tsx
+++ src/features/diagnosis/pages/DiagnosisPage.tsx
@@ -152,144 +152,155 @@ export function DiagnosisPage() {
             {/* rPPGMeasurement Item List */}
             <RppgMeasurementList measurementValue={measurement} />
 
-            {currentTab === "diagnosis" && (
-                //TODO: fragmnet -> 화면 비율 설정해두기
-                <>
-                    {state.status === "init" && (
-                        <div className="w-full h-full flex flex-col items-center gap-4 px-4">
-                            <h1 className="text-black text-5xl font-bold animate-pulse my-8">
-                                {t("chooseDiagnosisTypeLabel")}
-                            </h1>
+            <div className="h-5/6 flex flex-col">
+                {currentTab === "diagnosis" && (
+                    //TODO: fragmnet -> 화면 비율 설정해두기
+                    <>
+                        {state.status === "init" && (
+                            <div className="w-full h-full flex flex-col items-center gap-4 px-4">
+                                <h1 className="text-black text-5xl font-bold animate-pulse my-8">
+                                    {t("chooseDiagnosisTypeLabel")}
+                                </h1>
 
-                            <div className="w-full h-2/3 flex flex-row gap-4">
-                                <DiagnosisSelectableButton
-                                    isSelected={
-                                        selectedDiagnosis === "Depression"
-                                    }
-                                    onClick={() => {
-                                        if (
-                                            selectedDiagnosis !== "Depression"
-                                        ) {
-                                            setSelectedDiagnosis("Depression");
+                                <div className="w-full h-2/3 flex flex-row gap-4">
+                                    <DiagnosisSelectableButton
+                                        isSelected={
+                                            selectedDiagnosis === "Depression"
                                         }
-                                    }}
-                                    emojiSrc={Crying}
-                                    label={t("DepressionDiagnosisLabel")}
-                                    description={t("depressionDescription")}
-                                />
-                                <DiagnosisSelectableButton
-                                    isSelected={
-                                        selectedDiagnosis === "Dementia"
-                                    }
-                                    onClick={() => {
-                                        if (selectedDiagnosis !== "Dementia") {
-                                            setSelectedDiagnosis("Dementia");
+                                        onClick={() => {
+                                            if (
+                                                selectedDiagnosis !==
+                                                "Depression"
+                                            ) {
+                                                setSelectedDiagnosis(
+                                                    "Depression"
+                                                );
+                                            }
+                                        }}
+                                        emojiSrc={Crying}
+                                        label={t("DepressionDiagnosisLabel")}
+                                        description={t("depressionDescription")}
+                                    />
+                                    <DiagnosisSelectableButton
+                                        isSelected={
+                                            selectedDiagnosis === "Dementia"
                                         }
-                                    }}
-                                    emojiSrc={Thinking}
-                                    label={t("DementiaDiagnosisLabel")}
-                                    description={t("dementiaDescription")}
-                                />
-                            </div>
-                            <PrimaryButton
-                                label={t("btnStartDiagnosis")}
-                                onClick={() => {
-                                    let questions = DEPRESSIONQUESTIONS;
-                                    let options = DEPRESSIONOPTIONS;
-                                    if (selectedDiagnosis === "Dementia") {
-                                        questions = DEMENTIAQUESTIONS;
-                                        options = DEMENTIAOPTIONS;
-                                    }
-                                    startSurvey(questions, options);
-                                }}
-                            />
-                        </div>
-                    )}
-                    {state.status === "onProgress" && (
-                        <>
-                            <div className="w-full h-[13vh] mt-4 p-6 bg-white border-slate-300 rounded-lg">
-                                <div className="h-full flex flex-col justify-center items-center">
-                                    <div className="h-full" />
-                                    <h1 className="w-full h-1/2 text-center text-4xl font-bold text-black">
-                                        {
-                                            state.surveyQuestions.questions[
-                                                state.currentIndex
-                                            ]
-                                        }
-                                    </h1>
-                                    <div className="h-full" />
-                                    <p className="text-black">
-                                        {`${state.currentIndex + 1} / ${
-                                            state.surveyQuestions.questions
-                                                .length
-                                        }`}
-                                    </p>
-                                    <div className="h-full" />
+                                        onClick={() => {
+                                            if (
+                                                selectedDiagnosis !== "Dementia"
+                                            ) {
+                                                setSelectedDiagnosis(
+                                                    "Dementia"
+                                                );
+                                            }
+                                        }}
+                                        emojiSrc={Thinking}
+                                        label={t("DementiaDiagnosisLabel")}
+                                        description={t("dementiaDescription")}
+                                    />
                                 </div>
-                            </div>
-                            {state.surveyQuestions.options.map((_, idx) => (
-                                <AnswerButton
-                                    key={idx}
-                                    label={state.surveyQuestions.options[idx]}
-                                    isSelected={tappedButtonIdx === idx}
-                                    handleTap={() => handleAnswerTap(idx)}
-                                />
-                            ))}
-                            <div className="mt-[5vh] mb-[1vh] gap-0">
-                                <Button
-                                    w="30%"
-                                    h="3vh"
-                                    color="white"
-                                    fontSize="l"
-                                    fontWeight="bold"
-                                    bg={palettes.primary}
-                                    borderColor={palettes.primary}
-                                    borderWidth={2}
-                                    borderRadius={12}
+                                <PrimaryButton
+                                    label={t("btnStartDiagnosis")}
                                     onClick={() => {
-                                        if (state.currentIndex === 0) {
-                                            initSurvey();
+                                        let questions = DEPRESSIONQUESTIONS;
+                                        let options = DEPRESSIONOPTIONS;
+                                        if (selectedDiagnosis === "Dementia") {
+                                            questions = DEMENTIAQUESTIONS;
+                                            options = DEMENTIAOPTIONS;
                                         }
-                                        goBack();
+                                        startSurvey(questions, options);
                                     }}
+                                />
+                            </div>
+                        )}
+                        {state.status === "onProgress" && (
+                            <>
+                                <div className="w-full h-[13vh] mt-4 p-6 bg-white border-slate-300 rounded-lg">
+                                    <div className="h-full flex flex-col justify-center items-center">
+                                        <div className="h-full" />
+                                        <h1 className="w-full h-1/2 text-center text-4xl font-bold text-black">
+                                            {
+                                                state.surveyQuestions.questions[
+                                                    state.currentIndex
+                                                ]
+                                            }
+                                        </h1>
+                                        <div className="h-full" />
+                                        <p className="text-black">
+                                            {`${state.currentIndex + 1} / ${
+                                                state.surveyQuestions.questions
+                                                    .length
+                                            }`}
+                                        </p>
+                                        <div className="h-full" />
+                                    </div>
+                                </div>
+                                {state.surveyQuestions.options.map((_, idx) => (
+                                    <AnswerButton
+                                        key={idx}
+                                        label={
+                                            state.surveyQuestions.options[idx]
+                                        }
+                                        isSelected={tappedButtonIdx === idx}
+                                        handleTap={() => handleAnswerTap(idx)}
+                                    />
+                                ))}
+                                <div className="mt-[5vh] mb-[1vh] gap-0">
+                                    <Button
+                                        w="30%"
+                                        h="3vh"
+                                        color="white"
+                                        fontSize="l"
+                                        fontWeight="bold"
+                                        bg={palettes.primary}
+                                        borderColor={palettes.primary}
+                                        borderWidth={2}
+                                        borderRadius={12}
+                                        onClick={() => {
+                                            if (state.currentIndex === 0) {
+                                                initSurvey();
+                                            }
+                                            goBack();
+                                        }}
+                                        className="shadow"
+                                    >
+                                        {t("btnPrev")}
+                                    </Button>
+                                </div>
+                                <div className="h-[10vh]" />
+                                <Button
+                                    w="100%"
+                                    h="3vh"
+                                    bg="white"
+                                    fontWeight="bold"
+                                    borderColor={palettes.grey}
+                                    // borderWidth={2}
+                                    borderRadius={12}
+                                    color="black"
+                                    fontSize="l"
+                                    onClick={handleGoPreviousPage}
                                     className="shadow"
                                 >
-                                    {t("btnPrev")}
+                                    {t("btnStop")}
                                 </Button>
-                            </div>
-                            <div className="h-[10vh]" />
-                            <Button
-                                w="100%"
-                                h="3vh"
-                                bg="white"
-                                fontWeight="bold"
-                                borderColor={palettes.grey}
-                                // borderWidth={2}
-                                borderRadius={12}
-                                color="black"
-                                fontSize="l"
-                                onClick={handleGoPreviousPage}
-                                className="shadow"
-                            >
-                                {t("btnStop")}
-                            </Button>
-                        </>
-                    )}
-                    {state.status === "done" && (
-                        <DiagnosisDone
-                            rppgMesurement={measurement}
-                            hrValues={hrRef.current}
-                            selectedDiagnosisType={selectedDiagnosis}
-                            answers={state.responses}
-                        />
-                    )}
-                </>
-            )}
-            {/* Add Chat fragment here */}
-            {currentTab === "chat" && <ChatFragment />}
-            {currentTab === "dtx" && <DtxFragmentV2 />}
+                            </>
+                        )}
+                        {state.status === "done" && (
+                            <DiagnosisDone
+                                rppgMesurement={measurement}
+                                hrValues={hrRef.current}
+                                selectedDiagnosisType={selectedDiagnosis}
+                                answers={state.responses}
+                            />
+                        )}
+                    </>
+                )}
+                {/* Add Chat fragment here */}
+                {currentTab === "chat" && <ChatFragment />}
+                {currentTab === "dtx" && <DtxFragmentV2 />}
+            </div>
 
-            <div className="w-full h-1/5 flex flex-row items-center justify-between rounded-t-3xl bg-white shadow-2xl px-72">
+            <div className="w-full h-1/6 flex flex-row items-center justify-between rounded-t-3xl bg-white shadow-2xl px-72">
                 <BottomNavigationButton
                     label="Diagnosis"
                     isSelected={currentTab === "diagnosis"}
