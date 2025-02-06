c673d1b8a5221727801864011753d8f60c0a3db0
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-02-06 14:35:08 +0900

    Design : Add Scale animation to AnswerButton

--- src/components/custom/AnswerButton.tsx
+++ src/components/custom/AnswerButton.tsx
@@ -1,4 +1,5 @@
 import { clsx } from "clsx";
+import { motion } from "motion/react";
 
 interface AnswerButtonProps {
     label: string;
@@ -11,13 +12,14 @@ export function AnswerButton({
     isSelected,
     handleTap,
 }: AnswerButtonProps) {
-    const selectedClassName = "bg-blue-100 border-blue-500 shadow-sm";
+    const selectedClassName = "bg-blue-50 border-blue-500 shadow-lg";
     const unselectedClassName = "bg-white border-white";
     return (
-        <button
+        <motion.button
             onClick={handleTap}
+            whileTap={{ scale: 0.95 }}
             className={clsx(
-                "w-full flex flex-row  justify-center p-6 rounded-2xl mt-4 border",
+                "w-full flex flex-row justify-center p-6 rounded-2xl mt-4 border transition",
                 isSelected ? selectedClassName : unselectedClassName
             )}
         >
@@ -28,6 +30,6 @@ export function AnswerButton({
             >
                 {label}
             </h1>
-        </button>
+        </motion.button>
     );
 }
