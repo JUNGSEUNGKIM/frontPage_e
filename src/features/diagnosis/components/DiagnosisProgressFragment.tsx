b13c890e20a30e989206b2e2a36926fded308799
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-02-06 14:50:50 +0900

    Feat : Add Progress Indicator component to Diagnosis Progress Fragment

--- src/features/diagnosis/components/DiagnosisProgressFragment.tsx
+++ src/features/diagnosis/components/DiagnosisProgressFragment.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { AnswerButton } from "@/components/custom/AnswerButton";
 import { useDiagnosisStore } from "@/shared/stores/diagnosisStore";
 import DiagnosisProgressToolBar from "./DiagnosisProgressToolBar";
+import { motion } from "motion/react";
 
 export default function DiagnosisProgressFragment() {
     const { surveyState } = useDiagnosisStore();
@@ -17,12 +18,17 @@ export default function DiagnosisProgressFragment() {
         setTappedButtonIdx(idx);
     }
 
+    const progressPercentage =
+        (surveyState.currentIndex /
+            surveyState.surveyQuestions.questions.length) *
+        100;
+
     return (
         // fragment container
         <div className="w-full h-full flex flex-col justify-center px-8">
             {/* Question Text */}
-            <div className="h-1/4">
-                <h1 className="w-full text-center text-4xl font-bold text-black mt-14">
+            <div className="h-1/4 flex flex-col items-center gap-10">
+                <h1 className="w-full h-2/5 text-center text-4xl font-bold text-black mt-14">
                     {
                         surveyState.surveyQuestions.questions[
                             surveyState.currentIndex
@@ -30,6 +36,12 @@ export default function DiagnosisProgressFragment() {
                     }
                 </h1>
                 {/* Progress indicator */}
+                <div className="w-11/12 h-[0.4rem] bg-[#d9d9d9]">
+                    <motion.div
+                        animate={{ width: `${progressPercentage}%` }}
+                        className="h-full bg-blue-500"
+                    />
+                </div>
             </div>
 
             <div className="w-full h-2/4">
