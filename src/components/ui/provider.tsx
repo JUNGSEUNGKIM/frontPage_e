8b0e238bc50c9d9e09aeffdb87f1a1f026bfd56a
Kevin <kevinmin1031@gmail.com>
2025-02-21 15:22:05 +0900

    Fix: fixes #25 fixes 26

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -3,15 +3,30 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export function Provider(props: ColorModeProviderProps) {
     const queryClient = new QueryClient();
 
+    useEffect(() => {
+        const handleContextMenu = (e: any) => {
+          e.preventDefault()
+        }
+        document.addEventListener("contextmenu", handleContextMenu)
+        return () => {
+          document.removeEventListener("contextmenu", handleContextMenu)
+        }
+      }, [])
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <ChakraProvider value={defaultSystem}>
-                <ColorModeProvider {...props} forcedTheme="light" />
-            </ChakraProvider>
-        </QueryClientProvider>
+        <div 
+            style={{touchAction: 'none'}}
+        >
+            <QueryClientProvider client={queryClient}>
+                <ChakraProvider value={defaultSystem}>
+                    <ColorModeProvider {...props} forcedTheme="light" />
+                </ChakraProvider>
+            </QueryClientProvider>
+        </div>
     );
 }
